3f23e715521e34877d128c65055a2f8453a8b206
Hubertus <huberkl@arcor.de>
2025-01-24 16:29:40 +0100

    Signatur einlesen 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,58 +4,34 @@ import com.itextpdf.text.pdf.*;
 import com.itextpdf.text.*;
 
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Security;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main {
 
-    public static final String PUBLIC = "resources/misc/test.cer";
-    public static final String DEST = "hello.pdf";
+    public static final String DEST = "unsigned.pdf";
 
     public static void main(String[] args) throws IOException, DocumentException{
+        //createDocument(DEST);
+        checkDocument(DEST);
+    }
 
-        // step 1: Create a Document
+    private static void createDocument(String name)throws IOException, DocumentException{
         Document document = new Document();
-        // step 2: Create a PdfWriter
         PdfWriter writer = PdfWriter.getInstance(
-                document, new FileOutputStream(DEST));
-        // step 3: Open the Document
+                document, new FileOutputStream(name));
         document.open();
-        // step 4: Add content
         document.add(new Paragraph("Hello World..."));
-        // create a signature form field
-        /*PdfFormField field = PdfFormField.createSignature(writer);
-        field.setFieldName("huberkl");
-        // set the widget properties
-        field.setPage();
-        field.setWidget(
-                new Rectangle(72, 732, 144, 780), PdfAnnotation.HIGHLIGHT_INVERT);
-        field.setFlags(PdfAnnotation.FLAGS_PRINT);
-        // add it as an annotation
-        writer.addAnnotation(field);
-        // maybe you want to define an appearance
-        PdfAppearance tp = PdfAppearance.createAppearance(writer, 144, 96);
-        tp.setColorStroke(BaseColor.BLUE);
-        tp.setColorFill(BaseColor.LIGHT_GRAY);
-        tp.rectangle(0.5f, 0.5f, 71.5f, 47.5f);
-        tp.fillStroke();
-        tp.setColorFill(BaseColor.BLUE);
-        ColumnText.showTextAligned(tp, Element.ALIGN_CENTER,
-                new Phrase("SIGN HERE"), 36, 24, 25);
-        field.setAppearance(PdfAnnotation.APPEARANCE_NORMAL, tp);
-
-*/
-        // step 5: Close the Document
         document.close();
+    }
+
+    private static void checkDocument(String name)throws IOException, DocumentException{
+        PdfReader reader = new PdfReader(new FileInputStream(name));
+        AcroFields acroFields = reader.getAcroFields();
+        ArrayList<String> signatureNames = acroFields.getSignatureNames();
+        for (String s:signatureNames) System.out.println(s);
     }
 }
